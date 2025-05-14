a2da36c2ee4e9de47f518dfa5765abfea805ef7d
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-14 15:54:54 +0200

    Initial Commit

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Manyuchi, Carrington C on 2025/05/14.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
